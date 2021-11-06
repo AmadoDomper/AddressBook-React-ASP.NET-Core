6f9e97f7bb36b8fc1b04205cd6aee31c40b4a5d1
AmadoDomper <amado.domper@gmail.com>
2021-11-06 10:24:47 -0500

    Add DataAnnotations for validation

--- AddressBookAPI/Entities/Contact.cs
+++ AddressBookAPI/Entities/Contact.cs
@@ -1,10 +1,19 @@
-﻿namespace AddressBookAPI.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AddressBookAPI.Entities
 {
     public class Contact
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "The field with name {0} is required")]
+        [StringLength(100)]
         public string Name { get; set; }
+        [Required(ErrorMessage = "The field with name {0} is required")]
+        [StringLength(50)]
         public string Email { get; set; }
+        [Required(ErrorMessage = "The field with name {0} is required")]
+        [StringLength(20)]
         public string Phone { get; set; }
     }
 }
